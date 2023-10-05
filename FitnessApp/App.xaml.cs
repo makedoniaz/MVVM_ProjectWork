f1f877d2d5be59a6941e3770e9ca7202c60c9571
makedoniaz <jafar.sadig04@gmail.com>
2023-10-05 17:39:37 +0400

    Add ContentControl and set project startup in App.xaml.cs

--- FitnessApp/App.xaml.cs
+++ FitnessApp/App.xaml.cs
@@ -1,17 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FitnessApp.ViewModels.Base;
+using FitnessApp.ViewModels;
 using System.Windows;
+using SimpleInjector;
 
-namespace FitnessApp
+namespace FitnessApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public static Container Container { get; set; } = new Container();
+
+    protected override void OnStartup(StartupEventArgs e)
     {
+        this.RegisterContainer();
+        this.Start<HomeViewModel>();
+
+        base.OnStartup(e);
+    }
+
+    private void Start<T>() where T : ViewModelBase
+    {
+        var mainView = new MainWindow();
+        var mainViewModel = Container.GetInstance<MainViewModel>();
+        mainViewModel.ActiveViewModel = Container.GetInstance<T>();
+
+        mainView.DataContext = mainViewModel;
+
+        mainView.ShowDialog();
+    }
+
+    private void RegisterContainer()
+    {
+        Container.RegisterSingleton<MainViewModel>();
+        Container.RegisterSingleton<HomeViewModel>();
+        Container.Verify();
     }
 }
--- FitnessApp/ViewModels/HomeViewModel.cs
+++ FitnessApp/ViewModels/HomeViewModel.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FitnessApp.ViewModels.Base;
 
-namespace FitnessApp.ViewModels
+namespace FitnessApp.ViewModels;
+
+public class HomeViewModel : ViewModelBase
 {
-    internal class HomeViewModel
-    {
-    }
+
 }
