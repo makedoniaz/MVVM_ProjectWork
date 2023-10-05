60163e15335f028bf03f474b4c2f41d34d0d0d55
makedoniaz <jafar.sadig04@gmail.com>
2023-10-05 16:38:32 +0400

    Add packages

--- FitnessApp/Models/Product.cs
+++ FitnessApp/Models/Product.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace FitnessApp.Models
+namespace FitnessApp.Models;
+
+public class Product
 {
-    internal class Product
-    {
-    }
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(500)]
+    public required string Name { get; set; }
+
+    // per 100 gramms
+
+    [Range(0, 2000)]
+    public double Calories { get; set; }
+
+    [Range(0, 1000)]
+    public double Protein { get; set; }
+
+    [Range(0, 1000)]
+    public double Carbs { get; set; }
+
+    [Range(0, 1000)]
+    public double Fats { get; set; }
 }
