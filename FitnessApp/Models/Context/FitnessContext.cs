4eb8a980001e777546a6e29d6d8e6897de9f6caa
makedoniaz <jafar.sadig04@gmail.com>
2023-10-05 16:49:09 +0400

    Add DbContext and Products migration

---
+++ FitnessApp/Models/Context/FitnessContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FitnessApp.Models.Context;
+
+public class FitnessContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseSqlServer(connectionString: "Server=localhost;Database=FitnessDb; TrustServerCertificate=True; Trusted_Connection=True;");
+    }
+}
